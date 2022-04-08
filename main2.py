95f149976ee2b3c6e0f6ad1128cce7c6161efe3a
Gabriel <yanzeyu2019@gmail.com>
2022-04-08 22:42:03 +0800

    添加了sort跟踪方法，与deepsort可切换

--- main2.py
+++ main2.py
@@ -9,11 +9,15 @@
 """
 import time
 
+import numpy
+
 import tracker
 from detector import Detector
 import cv2
+from sort.sort import Sort
 
 if __name__ == '__main__':
+    sort=Sort()
     resize = (960, 540)
     list_bboxes = []
     counter = 0
@@ -21,8 +25,8 @@ if __name__ == '__main__':
     # 初始化 yolov5
     detector = Detector()
     # 打开视频
-    # capture = cv2.VideoCapture('./video/test.mp4')
-    capture = cv2.VideoCapture(1)
+    capture = cv2.VideoCapture('./video/test.mp4')
+    # capture = cv2.VideoCapture(0)
     start_time = time.time()
     while True:
         _, im = capture.read()
@@ -32,10 +36,18 @@ if __name__ == '__main__':
         bboxes = detector.detect(im)
         if len(bboxes) > 0:
             # before: [(123, 396, 179, 537, 'person', tensor(0.84004, device='cuda:0')), ...]
-            list_bboxes = tracker.update(bboxes, im)
+            for i in range(0, len(bboxes)):
+                list2 = list(bboxes[i][0:4])
+                list2.append(bboxes[i][5].item())
+                bboxes[i] = list2
+            list_bboxes = sort.update(numpy.array(bboxes))
+            list1 = list_bboxes.tolist()
+            for i in range(0, len(list1)):
+                list1[i] = (int(list1[i][0]), int(list1[i][1]), int(list1[i][2]), int(list1[i][3]), 'person', int(list1[i][4]))
+            # list_bboxes = tracker.update(bboxes, im)
             # after: [(121, 396, 180, 536, 'person', 1), (399, 116, 432, 211, 'person', 2), ...]
             # 画框
-            output_image_frame = tracker.draw_bboxes(im, list_bboxes, line_thickness=None)
+            output_image_frame = tracker.draw_bboxes(im, list1, line_thickness=None)
         else:
             output_image_frame = im
         counter += 1  # 计算帧数
